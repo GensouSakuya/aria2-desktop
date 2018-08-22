8c93c875dcbf99f962d3b48bcfa5932752b0b01c
GensouSakuya <zyqwert2@gmail.com>
2018-08-22 17:16:02 +0800

    add save config to file

---
+++ Core/Manager/ConfigManager.cs
@@ -0,0 +1,34 @@
+﻿using Model;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Core
+{
+    public static class ConfigManager
+    {
+        public static ConfigInfo GetFromFile(string path = ConfigConst.Default_Config_File_Path)
+        {
+            var configStr = "";
+            using (StreamReader file = File.OpenText(path))
+            {
+                configStr = file.ReadToEnd();
+            }
+
+            return JsonConvert.DeserializeObject<ConfigInfo>(configStr);
+        }
+
+        public static void SaveToFile(ConfigInfo config, string path = ConfigConst.Default_Config_File_Path)
+        {
+            var confStr = JsonConvert.SerializeObject(config);
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            using (StreamWriter stream = File.CreateText(path))
+            {
+                stream.Write(confStr);
+            }
+        }
+    }
+}
---
+++ Model/Const/ConfigConst.cs
@@ -0,0 +1,7 @@
+﻿namespace Model
+{
+    public static class ConfigConst
+    {
+        public const string Default_Config_File_Path = @"\Config\aria2.conf";
+    }
+}
