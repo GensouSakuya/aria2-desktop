906cab966f43e55ca7780e922a62e3df3311c3f2
GensouSakuya <zyqwert2@gmail.com>
2018-08-23 16:37:18 +0800

    change main to unstatic

--- Core/Main.cs
+++ Core/Main.cs
@@ -1,23 +1,55 @@
 ﻿using Model;
+using System;
 
 namespace Core
 {
-    public static class Main
+    public class Main: IDisposable
     {
-        private static ConfigInfo _config = null;
-
-        public static void Start()
+        public void Start(string configFilePath = null)
         {
+            _configFilePath = configFilePath;
+
             //1.获取配置
-            _config = ConfigManager.GetFromFile();
-            if (_config == null)
-            {
-                _config = ConfigManager.SaveToFile();
-            }
 
             //2.根据配置文件组装Aria2配置
 
             //3.使用配置启动Aria2
+        }
+
+        public void Dispose()
+        {
+            if (_config != null)
+            {
+                _config = ConfigManager.SaveToFile();
+            }
+        }
+
+
+        private volatile object lockObj = new object();
+        private ConfigInfo _config = null;
+        private string _configFilePath = null;
+
+        protected ConfigInfo Config
+        {
+            get
+            {
+                if (_config == null)
+                {
+                    lock (lockObj)
+                    {
+                        if (_config == null)
+                        {
+                            _config = ConfigManager.GetFromFile(_configFilePath);
+                            if (_config == null)
+                            {
+                                _config = ConfigManager.SaveToFile(path: _configFilePath);
+                            }
+                        }
+                    }
+                }
+
+                return _config;
+            }
         }
     }
 }
