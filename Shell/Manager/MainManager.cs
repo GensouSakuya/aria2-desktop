dc444f686c650f2536d70080c34459bd6e96b8c6
GensouSakuya <zyqwert2@gmail.com>
2018-08-24 12:36:22 +0800

    add app.start

---
+++ Shell/Manager/MainManager.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+
+namespace Shell
+{
+    public static class MainManager
+    {
+        public static Main Main { get; private set; } = null;
+        private static volatile object locker = null;
+
+        public static void StartUp()
+        {
+            if (Main == null)
+            {
+                lock (locker)
+                {
+                    if (Main == null)
+                    {
+                        Main = new Main();
+                    }
+                }
+            }
+        }
+    }
+}
