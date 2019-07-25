d53e57deaf66bc40b196b57100a960eb15f70dd9
GensouSakuya <zyqwert2@gmail.com>
2019-07-25 14:34:36 +0800

    增加aria2帮助类

---
+++ src/GensouSakuya.Aria2.Desktop.Shell/Helper/Aria2Helper.cs
@@ -0,0 +1,32 @@
+﻿using GensouSakuya.Aria2.Desktop.Core;
+
+namespace GensouSakuya.Aria2.Desktop.Shell.Helper
+{
+    public class Aria2Helper
+    {
+        public static Main Main { get; private set; } = null;
+        private static volatile object locker = new object();
+
+        public static void StartUp()
+        {
+            if (Main == null)
+            {
+                lock (locker)
+                {
+                    if (Main == null)
+                    {
+                        Main = new Main();
+                    }
+                }
+            }
+        }
+
+        public static void Shutdown()
+        {
+            if (Main != null)
+            {
+                Main.Shutdown();
+            }
+        }
+    }
+}
