e5979f110d02d06431521f2d5cf0a0751d788742
GensouSakuya <zyqwert2@gmail.com>
2019-07-31 18:59:31 +0800

    增加bitmaphelper

---
+++ src/GensouSakuya.Aria2.Desktop.Shell/Helper/BitmapHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace GensouSakuya.Aria2.Desktop.Shell.Helper
+{
+    public static class BitmapHelper
+    {
+        public static Bitmap GetImg(string assertPath)
+        {
+            var uri = new Uri($"avares://{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}/{assertPath}");
+
+            var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+            return new Bitmap(assets.Open(uri));
+        }
+    }
+}
