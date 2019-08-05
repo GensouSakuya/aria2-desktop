a1334d23cdf0eaec547e3050b094438208278a06
GensouSakuya <zyqwert2@gmail.com>
2019-08-05 18:10:31 +0800

    增加更多下载任务信息

--- src/GensouSakuya.Aria2.Desktop.Shell/Controls/ViewModels/DownloadTaskItemViewModel.cs
+++ src/GensouSakuya.Aria2.Desktop.Shell/Controls/ViewModels/DownloadTaskItemViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Diagnostics.ViewModels;
+﻿using System;
+using Avalonia.Diagnostics.ViewModels;
 using Avalonia.Media.Imaging;
 using GensouSakuya.Aria2.Desktop.Model;
 using GensouSakuya.Aria2.Desktop.Shell.Helper;
@@ -9,22 +10,44 @@ namespace GensouSakuya.Aria2.Desktop.Shell.Controls.ViewModels
     {
         public Bitmap Img { get; set; } = ImgResourceHelper.FileIcon;
         public string TaskName { get; set; } = "新任务";
-        public decimal Progress { get; set; } = 0m;
 
-        public decimal DownloadSpeed { get; set; } = 0m;
+        public DownloadStatus Status { get; set; }
 
-        public string DownloadSpeedStr
+        public decimal CompleteSize { get; set; }
+        public decimal LeftSize => TotalSize - CompleteSize;
+        public decimal LeftSeconds => DownloadSpeed > 0 ? LeftSize / DownloadSpeed : -1;
+
+        public string LeftTime
         {
             get
             {
-                if (DownloadSpeed <= 0)
+                if (Status != DownloadStatus.Active)
                 {
                     return "";
                 }
 
-                return Tools.ToStringWithUnit(DownloadSpeed) + "/s";
+                if (LeftSeconds <= 0)
+                {
+                    return "--:--:--";
+                }
+                else
+                {
+                    var timesplan = new TimeSpan(0, 0, (int) LeftSeconds);
+                    return
+                        $"{Math.Floor(timesplan.TotalHours).ToString("00")}:{Math.Floor(timesplan.TotalMinutes).ToString("00")}:{Math.Floor(timesplan.TotalSeconds).ToString("00")}";
+                }
             }
         }
+
+        public decimal TotalSize { get; set; }
+        public string TotalSizeStr => $"{Tools.ToStringWithUnit(TotalSize)}";
+
+        public decimal Progress { get; set; } = 0m;
+        public string ProgressStr => $"{Math.Round(Progress, 2).ToString("0.00")}%";
+
+        public decimal DownloadSpeed { get; set; } = 0m;
+        public string DownloadSpeedStr => Status != DownloadStatus.Active ? "" : Tools.ToStringWithUnit(DownloadSpeed) + "/s";
+
     }
 
     internal static class DownloadTaskListViewModelExtension
@@ -33,7 +56,12 @@ namespace GensouSakuya.Aria2.Desktop.Shell.Controls.ViewModels
         {
             return new DownloadTaskItemViewModel
             {
-                TaskName = task.TaskName, Progress = task.CompletePercent, DownloadSpeed = task.DownloadSpeed
+                TaskName = task.TaskName,
+                Progress = task.CompletePercent,
+                DownloadSpeed = task.DownloadSpeed,
+                Status = task.Status,
+                TotalSize = task.TotalLength,
+                CompleteSize = task.CompletedLength
             };
         }
     }
