2134ce1a26fd259bef4b2deff50082fe75fd943d
GensouSakuya <zyqwert2@gmail.com>
2019-08-05 14:34:53 +0800

    增加任务信息展示字段

--- src/GensouSakuya.Aria2.Desktop.Shell/Controls/ViewModels/DownloadTaskLabelViewModel.cs
+++ src/GensouSakuya.Aria2.Desktop.Shell/Controls/ViewModels/DownloadTaskLabelViewModel.cs
@@ -7,5 +7,7 @@ namespace GensouSakuya.Aria2.Desktop.Shell.Controls.ViewModels
     public class DownloadTaskLabelViewModel: ViewModelBase
     {
         public Bitmap Img { get; set; } = ImgResourceHelper.FileIcon;
+        public string TaskName { get; set; } = "新任务";
+        public decimal Progress { get; set; } = 0m;
     }
 }
