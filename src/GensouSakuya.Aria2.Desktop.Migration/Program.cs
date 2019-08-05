5df90c6c4ab466541261c06b48f887a6a672098d
GensouSakuya <zyqwert2@gmail.com>
2019-08-05 11:34:04 +0800

    增加Migration项目

---
+++ src/GensouSakuya.Aria2.Desktop.Migration/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GensouSakuya.Aria2.Desktop.Migration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //迁移命令
+            //Add-Migration Init -Project GensouSakuya.Aria2.Desktop.Model -StartUpProject GensouSakuya.Aria2.Desktop.Migration
+        }
+    }
+}
