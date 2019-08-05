cff022fd09fb9b24a9e2717576e997ab14695f2c
GensouSakuya <zyqwert2@gmail.com>
2019-08-05 12:29:20 +0800

    增加数据库操作方法

--- src/GensouSakuya.Aria2.Desktop.Core/Aria2Core.Database.cs
+++ src/GensouSakuya.Aria2.Desktop.Core/Aria2Core.Database.cs
@@ -14,5 +14,31 @@ namespace GensouSakuya.Aria2.Desktop.Core
                 return context.DownloadTasks.ToList();
             }
         }
+
+        //public void SaveChanges()
+        //{
+        //    using (var context = new DbContext())
+        //    {
+        //        context.SaveChanges();
+        //    }
+        //}
+
+        public void InsertTask(DownloadTask task)
+        {
+            using (var context = new DbContext())
+            {
+                context.DownloadTasks.Add(task);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateTask(DownloadTask task)
+        {
+            using (var context = new DbContext())
+            {
+                context.Entry<DownloadTask>(task).State = Microsoft.EntityFrameworkCore.EntityState.Modified; 
+                context.SaveChanges();   
+            }
+        }
     }
 }
