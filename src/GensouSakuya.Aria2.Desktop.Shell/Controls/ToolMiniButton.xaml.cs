4f7408aaca18ff42266473eb1d8019b6b2881bac
GensouSakuya <zyqwert2@gmail.com>
2019-08-07 17:13:17 +0800

    增加小按钮

---
+++ src/GensouSakuya.Aria2.Desktop.Shell/Controls/ToolMiniButton.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using GensouSakuya.Aria2.Desktop.Shell.Controls.ViewModels;
+
+namespace GensouSakuya.Aria2.Desktop.Shell.Controls
+{
+    public class ToolMiniButton : UserControl
+    {
+        public ToolMiniButton()
+        {
+            this.InitializeComponent();
+
+            Initialized += (sender, e) =>
+            {
+                Tapped += (sender2, e2) =>
+                {
+                    if (this.DataContext is ToolButtonViewModel)
+                    {
+                        var model = this.DataContext as ToolButtonViewModel;
+                        if (model.Click != null)
+                        {
+                            model.Click();
+                        }
+                    }
+                };
+            };
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
