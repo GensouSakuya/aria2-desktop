8889f9791099558d19910aa26585598b6951bf60
GensouSakuya <zyqwert2@gmail.com>
2019-08-12 18:33:46 +0800

    增加下载任务选中变色

--- src/GensouSakuya.Aria2.Desktop.Shell/Controls/DownloadTaskItem.xaml.cs
+++ src/GensouSakuya.Aria2.Desktop.Shell/Controls/DownloadTaskItem.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace GensouSakuya.Aria2.Desktop.Shell.Controls
 {
@@ -8,11 +10,30 @@ namespace GensouSakuya.Aria2.Desktop.Shell.Controls
         public DownloadTaskItem()
         {
             this.InitializeComponent();
+
+            Initialized += (sender, e) =>
+            {
+                PointerEnter += NewPointerEnter;
+                PointerLeave += NewPointerLeave;
+            };
+
+            Background = new SolidColorBrush(Colors.Gray, 0);
+
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+        
+        private void NewPointerEnter(object sender, PointerEventArgs e)
+        {
+            Background = new SolidColorBrush(Colors.Gray,0.24);
+        }
+
+        private void NewPointerLeave(object sender, PointerEventArgs e)
+        {
+            Background = new SolidColorBrush(Colors.Gray, 0);
         }
     }
 }
