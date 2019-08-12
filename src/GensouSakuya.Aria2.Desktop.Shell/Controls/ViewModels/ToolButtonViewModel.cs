f38b2c5b817cb26f3845ac256504230dfe2e185f
GensouSakuya <zyqwert2@gmail.com>
2019-08-12 18:41:13 +0800

    按钮viewmodel增加临时默认图片

--- src/GensouSakuya.Aria2.Desktop.Shell/Controls/ViewModels/ToolButtonViewModel.cs
+++ src/GensouSakuya.Aria2.Desktop.Shell/Controls/ViewModels/ToolButtonViewModel.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using Avalonia.Diagnostics.ViewModels;
-using Avalonia.Media.Imaging;
+using GensouSakuya.Aria2.Desktop.Shell.Helper;
 
 namespace GensouSakuya.Aria2.Desktop.Shell.Controls.ViewModels
 {
     public class ToolButtonViewModel: ViewModelBase
     {
         public Action Click { get; set; }
-        public object Img { get; set; }
+        public object Img { get; set; } = BitmapHelper.GetImg(Resource.Icons.Stop);
         public string Content { get; set; } = "";
     }
 }
