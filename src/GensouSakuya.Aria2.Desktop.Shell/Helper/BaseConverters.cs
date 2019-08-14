b461781cc7927bd509d701d11fb521b8134a76c6
GensouSakuya <zyqwert2@gmail.com>
2019-08-14 09:16:59 +0800

    增加一些基础converter

---
+++ src/GensouSakuya.Aria2.Desktop.Shell/Helper/BaseConverters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ReactiveUI;
+
+namespace GensouSakuya.Aria2.Desktop.Shell.Helper
+{
+
+    public abstract class FromDecimalConverter : IBindingTypeConverter
+    {
+        public int GetAffinityForObjects(Type fromType, Type toType)
+        {
+            if (fromType == typeof(decimal))
+            {
+                return 100;
+            }
+
+            return 0;
+        }
+
+        public abstract bool TryConvert(object from, Type toType, object conversionHint, out object result);
+    }
+    public class DecimalToDoubleConverter : FromDecimalConverter
+    {
+        public override bool TryConvert(object from, Type toType, object conversionHint, out object result)
+        {
+            try
+            {
+                result = Convert.ToDouble((decimal) from);
+            }
+            catch
+            {
+                result = null;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
