0cad3e504a4c4c05dd26d2db0b572664a9667af7
GensouSakuya <zyqwert2@gmail.com>
2019-08-14 17:26:06 +0800

    done

--- src/GensouSakuya.Aria2.Desktop.Shell/ViewModels/DownloadTaskListViewModel.cs
+++ src/GensouSakuya.Aria2.Desktop.Shell/ViewModels/DownloadTaskListViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Threading;
+using DynamicData;
+using DynamicData.Binding;
 using GensouSakuya.Aria2.Desktop.Shell.Controls.ViewModels;
 using GensouSakuya.Aria2.Desktop.Shell.Helper;
 using ReactiveUI;
@@ -14,7 +16,9 @@ namespace GensouSakuya.Aria2.Desktop.Shell.ViewModels
 
         public DownloadTaskListViewModel()
         {
-            Tasks = new ObservableCollection<DownloadTaskItemViewModel>();
+            ressssss = new ObservableCollectionExtended<DownloadTaskItemViewModel>(Aria2Helper.Aria2.DownloadTasks
+                .Select(p => p.ConvertToViewModel()).ToList());
+            ressssss.ToObservableChangeSet().Bind(out _tasks).Subscribe();
             DispatcherTimer.Run(() =>
             {
                 if (Aria2Helper.Aria2 == null)
@@ -22,22 +26,43 @@ namespace GensouSakuya.Aria2.Desktop.Shell.ViewModels
 
                 lock (refreshLock)
                 {
-                    var tasks = new ObservableCollection<DownloadTaskItemViewModel>(Aria2Helper.Aria2.DownloadTaskView
-                        .Select(p => p.ConvertToViewModel()).ToList());
+                    var tasks = Aria2Helper.Aria2.DownloadTasks;
+                    var allGid = tasks.Select(p => p.GID).Union(ressssss.Select(p => p.GID)).ToList();
 
-                    ViewModelListMerge(_tasks, tasks);
+                    allGid.ForEach(gid =>
+                    {
+                        var oriTask = ressssss.FirstOrDefault(p => p.GID == gid);
+                        var ar2Task = tasks.Find(p => p.GID == gid);
+
+                        if (oriTask == null)
+                        {
+                            ressssss.Add(ar2Task.ConvertToViewModel());
+                            return;
+                        }
+
+                        if (ar2Task == null)
+                        {
+                            ressssss.Remove(oriTask);
+                            return;
+                        }
+
+                        oriTask.TaskName = ar2Task.TaskName;
+                        oriTask.DownloadSpeed = ar2Task.DownloadSpeed;
+                        oriTask.Status = ar2Task.Status;
+                        oriTask.TotalSize = ar2Task.TotalLength;
+                        oriTask.CompleteSize = ar2Task.CompletedLength;
+                    });
+
+                    //ViewModelListMerge(_tasks, tasks);
                 }
 
                 return true;
             }, new TimeSpan(0, 0, 0, 0, 20));
         }
+        
+        private readonly ReadOnlyObservableCollection<DownloadTaskItemViewModel> _tasks;
+        public ReadOnlyObservableCollection<DownloadTaskItemViewModel> Tasks => _tasks;
 
-        public ObservableCollection<DownloadTaskItemViewModel> Tasks
-        {
-            get => _tasks;
-            set => this.RaiseAndSetIfChanged(ref _tasks, value);
-        }
-
-        private ObservableCollection<DownloadTaskItemViewModel> _tasks;
+        public ObservableCollectionExtended<DownloadTaskItemViewModel> ressssss { get; set; }
     }
 }
