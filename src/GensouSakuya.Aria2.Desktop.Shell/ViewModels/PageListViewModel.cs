9c53f973d018799ac9110d4a73adcac191cf4822
GensouSakuya <zyqwert2@gmail.com>
2019-08-16 19:13:07 +0800

    暂时用pagelist页测试一下新的iconbutton

---
+++ src/GensouSakuya.Aria2.Desktop.Shell/ViewModels/PageListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GensouSakuya.Aria2.Desktop.Shell.Controls.ViewModels;
+using ReactiveUI;
+
+namespace GensouSakuya.Aria2.Desktop.Shell.ViewModels
+{
+    public class PageListViewModel : ViewModelBase
+    {
+        public PageListViewModel()
+        {
+            IconModel = new IconButtonViewModel();
+        }
+        private IconButtonViewModel iconModel;
+
+        public IconButtonViewModel IconModel 
+        {
+            get { return iconModel; }
+            set { this.RaiseAndSetIfChanged(ref iconModel, value); }
+        }
+    }
+}
--- src/GensouSakuya.Aria2.Desktop.Shell/Views/PageList.xaml.cs
+++ src/GensouSakuya.Aria2.Desktop.Shell/Views/PageList.xaml.cs
@@ -1,13 +1,28 @@
-﻿using Avalonia;
+﻿using System.Reactive.Disposables;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using GensouSakuya.Aria2.Desktop.Shell.Controls;
+using GensouSakuya.Aria2.Desktop.Shell.Helper;
+using GensouSakuya.Aria2.Desktop.Shell.ViewModels;
+using ReactiveUI;
 
 namespace GensouSakuya.Aria2.Desktop.Shell.Views
 {
-    public class PageList : UserControl
+    public class PageList : ReactiveUserControl<PageListViewModel>
     {
+        public IconButton Icon2 => this.FindControl<IconButton>("ico"); 
         public PageList()
         {
+            this.WhenActivated(disposal =>
+            {
+                ViewModel = new PageListViewModel();
+                ViewModel.IconModel.Click = async () => { await DialogHelper.OpenFileDialog(null); };
+
+                this.OneWayBind(ViewModel, p => p.IconModel, p => p.Icon2.ViewModel)
+                    .DisposeWith(disposal);
+            });
+
             this.InitializeComponent();
         }
 
